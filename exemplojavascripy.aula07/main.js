0c5a742181598714d2b1bc2d3327555929f98d6d
Caualu <caualuizsilvacorrea@gmail.com>
2024-05-15 21:54:41 -0300

    aula.7
    aprendendo a incluir o login

---
+++ exemplojavascripy.aula07/main.js
@@ -0,0 +1,29 @@
+alert("ola, pequeno Warriors")
+console.log("Warriors")
+/*  var my_var;
+my_var='olá, pessoal'
+alert(my_var)
+my_var= 5
+alert(my_var)
+my_var = [1, 10, 'olá', true];
+alert(my_var)  */
+/* obs: operador ternário
+(my_var%2==0 ? alert('numero_par') : alert('valor impar')) 
+obs: tambem possui switch*/
+/*  var i;
+for (i=0; i<4; i++){
+    alert(i)
+}
+var i;
+i=0
+while (i<4){
+    alert(i)
+    i++
+}*/
+/* desvios imcondionadas :break e continue 
+(global)Var= Declarado em varias partes e ele pode mudar 
+(restrict)Const= declarado em varias partes so uma pode mudar 
+(private)let= so pode ser declarado em uma parte e nao muda o valor */
+
+var nome = prompt("Digite seu nome")
+alert("Ola " + nome);
