a4238566272e15d27800f1bdbd2cd6839930a67b
Caualu <caualuizsilvacorrea@gmail.com>
2024-05-23 11:44:53 -0300

    avaliação do carro

---
+++ avalição de revenda/ex01.py
@@ -0,0 +1,17 @@
+#Avaliação de carro
+carro1 =int(input("digite a nota do seu carro:"))
+carro2=int(input("digite o preço do seu carro:"))
+
+if carro1 >= 0 and carro1 <= 50:
+      valFinal = carro2- (carro2 * 30)/100
+      print(f"o preço do carro é: {valFinal}")
+elif carro1 > 50 and carro1 <- 80: 
+      print(f"o preço final do carro é {carro2}")
+elif carro1 > 80 and carro1 <- 100:
+      valFinal - carro2 + (carro2 * 20)/100
+      print(f"valFinal")      
+
+
+
+
+
